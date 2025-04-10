c5871e41b7f3ec4e404afa26f44b86bf8a40e4ed
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-10 14:49:30 +0700

    Add increment function, and state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+
+    // Create a state variable to store the value of the counter
+    // A state is a variable that the value  change and reflected in the UI
+    // Counter start from 0, button is pressed, counter increase by 1 and UI is refreshed
+
+    // counter -> to retrieve the value of the counter [getter]
+    // setCounter -> to set the value of the counter [setter]
+    // useState(0) -> 0 is the initial value of the counter
+    const [counter, setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter(counter + 1) // Increment the value of counter by 1
+    }
   return (
     <div>
         <h3>Counter</h3>
-        <h4>0</h4>
-        <button>Increment</button>
+        <h4>{counter}</h4>
+        <button onClick={increment}>Increment</button>
     </div>
   )
 }
