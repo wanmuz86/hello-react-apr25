8af23d6e8573b9f4562968e865a0fafeab4025b7
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-10 15:04:13 +0700

    Add message state, and apply it to decrement and reset at increment and reset

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,24 +9,40 @@ const Counter = () => {
     // counter -> to retrieve the value of the counter [getter]
     // setCounter -> to set the value of the counter [setter]
     // useState(0) -> 0 is the initial value of the counter
+
     const [counter, setCounter] = useState(0)
 
+    const [message, setMessage] = useState('') 
+
+    
     const increment = () => {
         setCounter(counter + 1) // Increment the value of counter by 1
+        setMessage('') // Reset the message
     }
+// Exercise: WHen user try to decrement the counter at 0, 
+// a message should be shown on the UI, eg below h4, 
+// Number cannot be less than 0, and it will go away when "Increment" or "Reset" is pressed
+
 
     const decrement = () => {
-        setCounter(counter-1) // Decrement the value of counter by 1
+        if (counter <= 0){
+            setMessage('Number cannot be less than 0')
+        }
+        else {
+            setCounter(counter-1) // Decrement the value of counter by 1
+        }
     }
 
     const reset = () => {
         setCounter(0)
+        setMessage('') // Reset the message
     }
 
   return (
     <div>
         <h3>Counter</h3>
         <h4>{counter}</h4>
+        <p>{message}</p>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
         <button onClick={reset}>Reset</button>
