4bdaadf50618489c6370f51c86c3c92efe75706a
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-10 14:06:20 +0700

    add components and call it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Main from "./components/Main"
+
 function App() {  
   return (
     <>
-      <h1>Hello React</h1>
-      <p>Welcome to my first React website</p>
+    {/* Calling the components in App.jsx */}
+     <Header/>
+     <Main/>
+     <Footer/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// with extension, just type rafce and press tab
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
