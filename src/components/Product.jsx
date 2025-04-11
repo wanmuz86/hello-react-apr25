e6d6ca32bb645f7f8165153b5b9fbc3b1181bd17
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-11 10:47:16 +0700

    show the product in UI

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const Product = () => {
+
+    const [product,setProduct] = useState(null)
 
     const retrieveProduct = () => {
     // Use then and catch to process async data (Promise)
     axios.get('https://dummyjson.com/products/1')
     .then(response=> {
         console.log(response.data)
+        setProduct(response.data)
     })
     .catch(error=>{
         console.log(error)
@@ -15,6 +18,18 @@ const Product = () => {
 }
   return (
     <div>
+        {
+            product?
+
+            <div>
+            <h4>{product.title}</h4>
+            <img src={product.thumbnail} alt={product.title} />
+            <h5>{product.category} - USD {product.price}</h5>
+            <p>{product.description}</p>
+            </div>
+            :
+            <p>No product yet</p>
+        }
         <button onClick={retrieveProduct}>Retrieve Product</button>
     </div>
   )
