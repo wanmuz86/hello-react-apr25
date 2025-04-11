b6bbf08afd835ef034e93e1d426082b76c50e83a
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-11 11:21:56 +0700

    Add fetch api and async await example

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,56 +3,98 @@ import axios from 'axios'
 
 const Product = () => {
 
-    const [product,setProduct] = useState(null)
-    const [loading,setLoading] = useState(false)
+    const [product, setProduct] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     // Is called when the component is mounted
-    useEffect(()=>{
+    useEffect(() => {
         // call the setup for the componenet
-        retrieveProduct()
+        retrievePoductAsyncAwait()
     }
-    ,[]) // call once
+        , []) // call once
 
+
+
+    const retrieveProductFetch = () => {
+        const productId = Math.ceil(Math.random() * 194) // Generate random number from 1 to 194
+
+        fetch(`https://dummyjson.com/products/${productId}`)
+            .then(response => response.json()) // transfom to json
+            .then(data => console.log(data.data)) // process data
+            .catch(err => console.log(err))
+    }
+
+    const retrieveProductFetchAsyncAwait = async () => {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+        try {
+            const productId = Math.ceil(Math.random() * 194) // Generate random number from 1 to 194    
+            const response = await fetch(`https://dummyjson.com/products/${productId}`)
+            const data = response.json()
+            console.log(data)
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
+    // Add async to the nearest function declaration
+    const retrievePoductAsyncAwait = async () => {
+
+        try {
+            setLoading(true)
+            const productId = Math.ceil(Math.random() * 194) // Generate random number from 1 to 194
+
+            const response = await axios.get(`https://dummyjson.com/products/${productId}`)
+            setProduct(response.data)
+            setLoading(false)
+        }
+        catch (e) {
+
+            console.log(e.message)
+            setLoading(false)
+        }
+
+    }
 
     const retrieveProduct = () => {
-    // 1) Use then and catch to process async data (Promise)
+        // 1) Use then and catch to process async data (Promise)
 
-    setLoading(true)
+        setLoading(true)
 
-    const productId = Math.ceil(Math.random() * 194) // Generate random number from 1 to 194
-    axios.get(`https://dummyjson.com/products/${productId}`)
-    .then(response=> {
-        console.log(response.data)
-        setProduct(response.data)
-        setLoading(false)
-    })
-    .catch(error=>{
-        console.log(error)
-        setLoading(false)
-    })
+        const productId = Math.ceil(Math.random() * 194) // Generate random number from 1 to 194
+        axios.get(`https://dummyjson.com/products/${productId}`)
+            .then(response => {
+                // success
+                console.log(response.data)
+                setProduct(response.data)
+                setLoading(false)
+            })
+            .catch(error => {
+                // error
+                console.log(error)
+                setLoading(false)
+            })
 
-    // 2) 
-}
-  return (
-    <div>
-         {
-            loading && <p>Loading...</p>
-        }
-        {
-            product?
-            <div>
-            <h4>{product.title}</h4>
-            <img src={product.thumbnail} alt={product.title} />
-            <h5>{product.category} - USD {product.price}</h5>
-            <p>{product.description}</p>
-            </div>
-            :
-            <p>No product yet</p>
-        }
-       
-        <button onClick={retrieveProduct} disabled={loading}>Retrieve New Product</button>
-    </div>
-  )
+        // 2) 
+    }
+    return (
+        <div>
+            {
+                loading && <p>Loading...</p>
+            }
+            {
+                product ?
+                    <div>
+                        <h4>{product.title}</h4>
+                        <img src={product.thumbnail} alt={product.title} />
+                        <h5>{product.category} - USD {product.price}</h5>
+                        <p>{product.description}</p>
+                    </div>
+                    :
+                    <p>No product yet</p>
+            }
+
+            <button onClick={retrievePoductAsyncAwait} disabled={loading}>Retrieve New Product</button>
+        </div>
+    )
 }
 
 export default Product
