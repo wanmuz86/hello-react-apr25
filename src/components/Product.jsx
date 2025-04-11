f10ca7c3ffbd88a73cea950a3454a541363f60c6
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-11 11:00:56 +0700

    Add useEffect in the component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Product = () => {
 
     const [product,setProduct] = useState(null)
+    const [loading,setLoading] = useState(false)
+
+    // Is called when the component is mounted
+    useEffect(()=>{
+        // call the setup for the componenet
+        retrieveProduct()
+    }
+    ,[]) // call once
+
 
     const retrieveProduct = () => {
-    // Use then and catch to process async data (Promise)
-    axios.get('https://dummyjson.com/products/1')
+    // 1) Use then and catch to process async data (Promise)
+
+    setLoading(true)
+
+    const productId = Math.ceil(Math.random() * 194) // Generate random number from 1 to 194
+    axios.get(`https://dummyjson.com/products/${productId}`)
     .then(response=> {
         console.log(response.data)
         setProduct(response.data)
+        setLoading(false)
     })
     .catch(error=>{
         console.log(error)
+        setLoading(false)
     })
+
+    // 2) 
 }
   return (
     <div>
+         {
+            loading && <p>Loading...</p>
+        }
         {
             product?
-
             <div>
             <h4>{product.title}</h4>
             <img src={product.thumbnail} alt={product.title} />
@@ -30,7 +49,8 @@ const Product = () => {
             :
             <p>No product yet</p>
         }
-        <button onClick={retrieveProduct}>Retrieve Product</button>
+       
+        <button onClick={retrieveProduct} disabled={loading}>Retrieve New Product</button>
     </div>
   )
 }
